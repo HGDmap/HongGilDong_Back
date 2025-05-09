45c08b3c2b9dd1644dc688b02c4a0c3a0111aba5
lee-youn <5816654@naver.com>
2025-05-09 21:12:58 +0900

    feat: initial project setup

---
+++ src/main/java/hongik/map/honggildong/HongGilDongApplication.java
@@ -0,0 +1,17 @@
+package hongik.map.honggildong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+public class HongGilDongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongGilDongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongik/map/honggildong/HongGilDongApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.map.honggildong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongGilDongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
