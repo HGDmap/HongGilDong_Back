572c6e4ca0e73832358ab823f65c99947cfe8686
challonsy <musain2019@naver.com>
2025-09-08 21:49:34 +0900

    #3 feat : node 도메인 세팅

---
+++ src/main/java/hongik/map/honggildong/domain/node/entity/Floor.java
@@ -0,0 +1,5 @@
+package hongik.map.honggildong.domain.node.entity;
+
+public enum Floor {
+    B5,B4,B3,B2,B1,F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12;
+}
--- src/main/java/hongik/map/honggildong/domain/node/entity/Node.java
+++ src/main/java/hongik/map/honggildong/domain/node/entity/Node.java
@@ -1,13 +1,24 @@
 package hongik.map.honggildong.domain.node.entity;
 
+import hongik.map.honggildong.domain.building.entity.Building;
 import hongik.map.honggildong.global.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Node extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Building building;
+
+    private NodeCode code;
+
+    private Double latitude;
+    private Double longitude;
+    //상대 고도
+    private Long height;
+    //건물 내 층수
+    private Floor floor;
+
 }
