0a266b941b5bcf34fc2209220ba6cb7cf134aaba
challonsy <musain2019@naver.com>
2025-09-09 17:33:48 +0900

    #3 feat : review 도메인 세팅

--- src/main/java/hongik/map/honggildong/domain/review/entity/Review.java
+++ src/main/java/hongik/map/honggildong/domain/review/entity/Review.java
@@ -1,14 +1,25 @@
 package hongik.map.honggildong.domain.review.entity;
 
+import hongik.map.honggildong.domain.facility.entity.Facility;
+import hongik.map.honggildong.domain.member.entity.Member;
 import hongik.map.honggildong.global.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Review extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Facility facility;
+
+    private String content;
+
+    private Integer rating;
+
+    //이미지 엔티티 or elementCollection 결정 필요
 }
