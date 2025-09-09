2d282149d22d269e2fc2915c2e8a085b95c883cf
challonsy <musain2019@naver.com>
2025-09-09 17:26:33 +0900

    #3 feat : facility 도메인 세팅

--- src/main/java/hongik/map/honggildong/domain/facility/entity/Facility.java
+++ src/main/java/hongik/map/honggildong/domain/facility/entity/Facility.java
@@ -1,14 +1,28 @@
 package hongik.map.honggildong.domain.facility.entity;
 
+import hongik.map.honggildong.domain.node.entity.Node;
 import hongik.map.honggildong.global.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Facility extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Node node;
+
+    private String name;
+    private String locationDetail;
+
+    @Enumerated(EnumType.STRING)
+    private FacilityType type;
+    @Enumerated(EnumType.STRING)
+    private HashTag hashTag;
+
+    private String phone;
+
+    private String openInfo;
+
 }
---
+++ src/main/java/hongik/map/honggildong/domain/facility/entity/HashTag.java
@@ -0,0 +1,5 @@
+package hongik.map.honggildong.domain.facility.entity;
+
+public enum HashTag {
+    REST, STUDY
+}
