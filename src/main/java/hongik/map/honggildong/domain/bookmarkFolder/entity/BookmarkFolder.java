eed224ae90b028a705d6db3c45ddb04fde81253a
challonsy <musain2019@naver.com>
2025-09-09 17:54:54 +0900

    #3 feat : bookmarkFolder 도메인 세팅

--- src/main/java/hongik/map/honggildong/domain/bookmarkFolder/entity/BookmarkFolder.java
+++ src/main/java/hongik/map/honggildong/domain/bookmarkFolder/entity/BookmarkFolder.java
@@ -1,14 +1,20 @@
 package hongik.map.honggildong.domain.bookmarkFolder.entity;
 
+import hongik.map.honggildong.domain.member.entity.Member;
 import hongik.map.honggildong.global.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class BookmarkFolder extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    //북마크 시설 추가 예정..
 }
