fd356fb8b80976cc0bdb26c72e9a3a51c0048df3
challonsy <musain2019@naver.com>
2025-09-13 14:49:23 +0900

    #3 feat : event 도메인 세팅

--- src/main/java/hongik/map/honggildong/domain/event/entity/Event.java
+++ src/main/java/hongik/map/honggildong/domain/event/entity/Event.java
@@ -1,14 +1,23 @@
 package hongik.map.honggildong.domain.event.entity;
 
+import hongik.map.honggildong.domain.node.entity.Node;
 import hongik.map.honggildong.global.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Event extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Node node;
+
+    private String name;
+    private String description;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
