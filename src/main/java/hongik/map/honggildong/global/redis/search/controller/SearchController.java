f3407fd059a43469e235b40441111cdbe87d97af
challonsy <musain2019@naver.com>
2025-10-27 00:03:06 +0900

    #22 feat : 검색 버튼 컨트롤러단 구현

--- src/main/java/hongik/map/honggildong/global/redis/search/controller/SearchController.java
+++ src/main/java/hongik/map/honggildong/global/redis/search/controller/SearchController.java
@@ -1,13 +1,16 @@
 package hongik.map.honggildong.global.redis.search.controller;
 
 
+import hongik.map.honggildong.domain.member.entity.Member;
 import hongik.map.honggildong.global.apiPayload.ApiResponse;
 import hongik.map.honggildong.global.redis.search.SearchIndexCleaner;
 import hongik.map.honggildong.global.redis.search.SearchIndexInitializer;
 import hongik.map.honggildong.global.redis.search.dto.SearchResultDTO;
-import hongik.map.honggildong.global.redis.search.service.SearchDataLoader;
 import hongik.map.honggildong.global.redis.search.service.SearchService;
+import hongik.map.honggildong.global.security.service.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,15 +28,31 @@ public class SearchController {
     private final SearchIndexCleaner cleaner;
 
     @GetMapping("/list/{query}")
-    public ApiResponse<List<SearchResultDTO.General>> search(@PathVariable("query") String query) {
+    @Operation(summary = "검색 시 자동완성 리스트 제공")
+    public ApiResponse<List<SearchResultDTO.AutoCompleteGeneral>> searchList(@PathVariable("query") String query) {
 
-        List<SearchResultDTO.General> body = searchService.search(query);
+        List<SearchResultDTO.AutoCompleteGeneral> body = searchService.autoComplete(query);
+
+        return ApiResponse.onSuccess(body);
+    }
+
+    @GetMapping("/{query}")
+    @Operation(summary = "검색 버튼 눌렀을 때", description = "특정 자동완성 안 누르고 바로 검색 버튼 누를 시")
+    public ApiResponse<SearchResultDTO.resultList> search(@PathVariable("query") String query,
+                                                          @AuthenticationPrincipal CustomUserDetails userDetails){
+        Member member=null;
+
+        if(userDetails != null){
+            member = userDetails.getMember(); //주의: 현재 Member는 컨텍스트에 등록된 객체가 아닌 POJO
+        }
+        SearchResultDTO.resultList body = searchService.search(query, member);
 
         return ApiResponse.onSuccess(body);
     }
 
     //검색어 데이터 추가는 시작 시 자동으로
     @GetMapping("/load")
+    @Operation(summary = "인덱스 삭제 후 인덱스 생성, 데이터까지 등록")
     public ApiResponse<String> load() {
 
         cleaner.cleanUp();
@@ -43,6 +62,7 @@ public class SearchController {
     }
 
     @GetMapping("/indexed-data")
+    @Operation(summary = "인덱싱 된 데이터 확인 용")
     public ApiResponse<List<String>> findAllIndexedData() {
 
         List<String> body = searchService.findAllIndexedData();
