9290ef8a03d03b076cc4fb38cda27a5b8435d664
challonsy <musain2019@naver.com>
2025-11-15 17:35:35 +0900

    #35 feat :  빌딩 층 별 시설 리스트 추가 제공

--- src/main/java/hongik/map/honggildong/domain/building/repository/BuildingRepository.java
+++ src/main/java/hongik/map/honggildong/domain/building/repository/BuildingRepository.java
@@ -2,6 +2,19 @@ package hongik.map.honggildong.domain.building.repository;
 
 import hongik.map.honggildong.domain.building.entity.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BuildingRepository extends JpaRepository<Building, Long> {
+
+    @Query("""
+    SELECT DISTINCT b
+    FROM Building b
+    LEFT JOIN FETCH b.facilities
+    WHERE b.id = :buildingId
+    """)
+    Optional<Building> findWithFacilitiesById(@Param("id") Long buildingId);
+
 }
